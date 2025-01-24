f4655da8f45a4c45e03d2342f4d005ef561cf69b
Pawkooo13 <strzalecki9@wp.pl>
2025-01-24 20:35:34 +0100

    added config.py file

---
+++ src/config.py
@@ -0,0 +1,6 @@
+import os
+
+DATA_DIR = 'data/' #os.path.join(os.getcwd(), 'data') #'data/'
+
+IMAGES_DIR = 'data/images' #os.path.join(DATA_DIR, 'images') #'data/images'
+MASKS_DIR = 'data/masks' #os.path.join(DATA_DIR, 'masks') #'data/masks'
