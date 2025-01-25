ed645f8d90db7dff2ca67b6895bd2111b7b53289
DagaKurk <dagmara.kurkowska00@gmail.com>
2025-01-25 21:42:35 +0100

    dodalam predykcje i dokladnosc modeli na zbiorze testowym

--- src/train.py
+++ src/train.py
@@ -4,40 +4,66 @@ import numpy as np
 from unet import UNet
 from fcn import FCN
 
-data_path = os.path.join(cfg.DATA_DIR, 'splitted_data.npy')
+def make_predictions(model, X_test):
+    preds = []
 
-X_train, Y_train, X_test, Y_test = np.load(data_path, allow_pickle=True)
+    for img in X_test:
+        pred = model.predict(img.reshape(1,512,512,3))
+        class_mask = np.argmax(pred, axis=-1)
+        preds.append(class_mask.reshape(512,512,1))
 
-# uczenie modelu unet
+    return preds
 
-print("Inicjalizowanie modelu UNet! \n")
+def get_accuracy(y_true, y_pred):
+    metric = tf.keras.metrics.Accuracy()
+    metric.update_state(y_true, y_pred)
+    return metric.result()
 
-unet_model = UNet.build_model()
+def main():
 
-unet_model.compile(optimizer='adam',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
+    data_path = os.path.join(cfg.DATA_DIR, 'splitted_data.npy')
 
-print("Uczenie modelu UNet: \n")
+    X_train, Y_train, X_test, Y_test = np.load(data_path, allow_pickle=True)
 
-unet_history = unet_model.fit(x=X_train,
-                              y=Y_train, 
-                              batch_size=4,
-                              epochs=100)
+    # uczenie modelu unet
 
-#uczenie modelu fcn
+    print("Inicjalizowanie modelu UNet! \n")
 
-print("Inicjalizowanie modelu FCN! \n")
+    unet_model = UNet.build_model()
 
-fcn_model = FCN.build_model()
+    unet_model.compile(optimizer='adam',
+                       loss='sparse_categorical_crossentropy',
+                       metrics=['accuracy'])
 
-fcn_model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+    print("Uczenie modelu UNet: \n")
 
-print("Uczenie modelu FCN: \n")
+    unet_history = unet_model.fit(x=X_train,
+                                  y=Y_train, 
+                                  batch_size=4,
+                                  epochs=100)
+    
+    unet_preds = make_predictions(model=unet_model, X_test=X_test)
+    unet_accuracy = get_accuracy(y_true=Y_test, y_pred=unet_preds)
 
-fcn_history = fcn_model.fit(x=X_train,
-                            y=Y_train,
-                            batch_size=4,
-                            epochs=100)
+    #uczenie modelu fcn
+
+    print("Inicjalizowanie modelu FCN! \n")
+
+    fcn_model = FCN.build_model()
+
+    fcn_model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+    print("Uczenie modelu FCN: \n")
+
+    fcn_history = fcn_model.fit(x=X_train,
+                                y=Y_train,
+                                batch_size=4,
+                                epochs=100)
+    
+    fcn_preds = make_predictions(model=fcn_model, X_test=X_test)
+    fcn_accuracy = get_accuracy(y_true=Y_test, y_pred=fcn_preds)
+    
+if __name__ == '__main__':
+    main()
