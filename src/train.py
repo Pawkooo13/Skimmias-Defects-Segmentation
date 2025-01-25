0f3d26d91cf9dde71682fba7caa336888fd31db3
DagaKurk <dagmara.kurkowska00@gmail.com>
2025-01-25 20:41:28 +0100

    dodalam printy o uczeniu konkretnego modelu

--- src/train.py
+++ src/train.py
@@ -8,22 +8,34 @@ data_path = os.path.join(cfg.DATA_DIR, 'splitted_data.npy')
 
 X_train, Y_train, X_test, Y_test = np.load(data_path, allow_pickle=True)
 
+# uczenie modelu unet
+
+print("Inicjalizowanie modelu UNet! \n")
+
 unet_model = UNet.build_model()
 
 unet_model.compile(optimizer='adam',
                    loss='sparse_categorical_crossentropy',
                    metrics=['accuracy'])
 
+print("Uczenie modelu UNet: \n")
+
 unet_history = unet_model.fit(x=X_train,
                               y=Y_train, 
                               batch_size=4,
                               epochs=100)
+
+#uczenie modelu fcn
+
+print("Inicjalizowanie modelu FCN! \n")
 
 fcn_model = FCN.build_model()
 
 fcn_model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
+
+print("Uczenie modelu FCN: \n")
 
 fcn_history = fcn_model.fit(x=X_train,
                             y=Y_train,
