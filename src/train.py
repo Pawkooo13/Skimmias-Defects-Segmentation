bbb9503cf184aee9cecb164aca11f5b3a74451e1
DagaKurk <dagmara.kurkowska00@gmail.com>
2025-01-25 22:02:44 +0100

    dodalam trenowanie modeli na zbiorze danych bez zdj hard mining + wyswietlenie wynikow w tabeli

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from unet import UNet
 from fcn import FCN
+import pandas as pd
 
 def make_predictions(model, X_test):
     preds = []
@@ -22,8 +23,10 @@ def get_accuracy(y_true, y_pred):
 def main():
 
     data_path = os.path.join(cfg.DATA_DIR, 'splitted_data.npy')
+    data_no_hard_mining_path = os.path.join(cfg.DATA_DIR, 'splitted_no_hard_mining_data.npy')
 
     X_train, Y_train, X_test, Y_test = np.load(data_path, allow_pickle=True)
+    X_train_2, Y_train_2, X_test_2, Y_test_2, = np.load(data_no_hard_mining_path, allow_pickle=True)
 
     # uczenie modelu unet
 
@@ -65,5 +68,50 @@ def main():
     fcn_preds = make_predictions(model=fcn_model, X_test=X_test)
     fcn_accuracy = get_accuracy(y_true=Y_test, y_pred=fcn_preds)
     
+    # uczenie modelu unet na zbiorze danych bez zdj hard mining
+
+    print("Inicjalizowanie modelu UNet! \n")
+
+    unet_model_2 = UNet.build_model()
+
+    unet_model_2.compile(optimizer='adam',
+                         loss='sparse_categorical_crossentropy',
+                         metrics=['accuracy'])
+
+    print("Uczenie modelu UNet na zbiorze danych bez zdj hard mining: \n")
+
+    unet_2_history = unet_model_2.fit(x=X_train,
+                                      y=Y_train, 
+                                      batch_size=4,
+                                      epochs=100)
+    
+    unet_2_preds = make_predictions(model=unet_model_2, X_test=X_test)
+    unet_2_accuracy = get_accuracy(y_true=Y_test, y_pred=unet_2_preds)
+
+    # uczenie modelu fcn na zbiorze dannych bez zdj hard mining
+
+    print("Inicjalizowanie modelu FCN! \n")
+
+    fcn_model_2 = FCN.build_model()
+
+    fcn_model_2.compile(optimizer='adam',
+                        loss='sparse_categorical_crossentropy',
+                        metrics=['accuracy'])
+
+    print("Uczenie modelu FCN: \n")
+
+    fcn_2_history = fcn_model_2.fit(x=X_train,
+                                    y=Y_train,
+                                    batch_size=4,
+                                    epochs=100)
+    
+    fcn_2_preds = make_predictions(model=fcn_model_2, X_test=X_test)
+    fcn_2_accuracy = get_accuracy(y_true=Y_test, y_pred=fcn_2_preds)
+
+    accuracies = pd.DataFrame(data={'model': ['UNet', 'FCN', 'UNet_2', 'FCN_2'],
+                                    'acc': [unet_accuracy, fcn_accuracy, unet_2_accuracy, fcn_2_accuracy]})
+    
+    print(accuracies.sort_values(by='acc', ascending=False))
+
 if __name__ == '__main__':
     main()
