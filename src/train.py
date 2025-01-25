e30536ceae1c12eebfbc9bdfa73228db90920b44
Pawkooo13 <strzalecki9@wp.pl>
2025-01-25 19:36:43 +0100

    added unet compile and fit functions

--- src/train.py
+++ src/train.py
@@ -1,10 +1,19 @@
 import config as cfg
 import os
 import numpy as np
-
+from unet import UNet
 
 data_path = os.path.join(cfg.DATA_DIR, 'splitted_data.npy')
 
 X_train, Y_train, X_test, Y_test = np.load(data_path, allow_pickle=True)
 
-print(X_train.shape, X_test.shape)
+unet_model = UNet.build_model()
+
+unet_model.compile(optimizer='adam',
+                   loss='sparse_categorical_crossentropy',
+                   metrics=['accuracy'])
+
+unet_history = unet_model.fit(x=X_train,
+                              y=Y_train, 
+                              batch_size=4,
+                              epochs=100)
